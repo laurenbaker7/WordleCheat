55200be2aa652b9a402d81febdaff7ad39fd3f87
Lauren Baker <laurenbaker@berkeley.edu>
2023-01-19 14:01:52 -0800

    deleted unused functions

--- wordleGraphics.py
+++ wordleGraphics.py
@@ -2,11 +2,7 @@ from tkinter import *
 from tkinter import ttk
 
 import sys
-import math
-import random
-import string
 import time
-import types
 import tkinter
 import os.path
 from wordleMain import *
@@ -19,32 +15,17 @@ _canvas_xs = None      # Size of canvas object
 _canvas_ys = None
 _canvas_x = None      # Current position on canvas
 _canvas_y = None
-_canvas_col = None      # Current colour (set to black below)
-_canvas_tsize = 12
-_canvas_tserifs = 0
 letterDict = {}
 allWords = None
 
 def formatColor(r, g, b):
     return '#%02x%02x%02x' % (int(r * 255), int(g * 255), int(b * 255))
 
-def colorToVector(color):
-    return list(map(lambda x: int(x, 16) / 256.0, [color[1:3], color[3:5], color[5:7]]))
-
 if _Windows:
     _canvas_tfonts = ['times new roman', 'lucida console']
 else:
     _canvas_tfonts = ['times', 'lucidasans-24']
     pass # XXX need defaults here
-
-def sleep(secs):
-    global _root_window
-    if _root_window == None:
-        time.sleep(secs)
-    else:
-        _root_window.update_idletasks()
-        _root_window.after(int(1000 * secs), _root_window.quit)
-        _root_window.mainloop()
 
 def begin_graphics(width=1200, height=700, color=formatColor(193, 193, 193), title=None):
 
@@ -104,24 +85,6 @@ def _ctrl_leftclick(event):
     global _ctrl_leftclick_loc
     _ctrl_leftclick_loc = (event.x, event.y)
 
-def wait_for_click():
-    while True:
-        global _leftclick_loc
-        global _rightclick_loc
-        global _ctrl_leftclick_loc
-        if _leftclick_loc != None:
-            val = _leftclick_loc
-            _leftclick_loc = None
-            return val, 'left'
-        if _rightclick_loc != None:
-            val = _rightclick_loc
-            _rightclick_loc = None
-            return val, 'right'
-        if _ctrl_leftclick_loc != None:
-            val = _ctrl_leftclick_loc
-            _ctrl_leftclick_loc = None
-            return val, 'ctrl_left'
-        sleep(0.05)
 
 def draw_background():
     corners = [(0,0), (0, _canvas_ys), (_canvas_xs, _canvas_ys), (_canvas_xs, 0)]
@@ -169,74 +132,14 @@ def polygon(coords, outlineColor, fillColor=None, filled=1, smoothed=1, behind=0
         _canvas.tag_lower(poly, behind) # Higher should be more visible
     return poly
 
-def square(pos, r, color, filled=1, behind=0):
-    x, y = pos
-    coords = [(x - r, y - r), (x + r, y - r), (x + r, y + r), (x - r, y + r)]
-    return polygon(coords, color, color, filled, 0, behind=behind)
-
-def circle(pos, r, outlineColor, fillColor=None, endpoints=None, style='pieslice', width=2):
-    x, y = pos
-    x0, x1 = x - r - 1, x + r
-    y0, y1 = y - r - 1, y + r
-    if endpoints == None:
-        e = [0, 359]
-    else:
-        e = list(endpoints)
-    while e[0] > e[1]: e[1] = e[1] + 360
-
-    return _canvas.create_arc(x0, y0, x1, y1, outline=outlineColor, fill=fillColor or outlineColor,
-                              extent=e[1] - e[0], start=e[0], style=style, width=width)
-
-def image(pos, file="../../blueghost.gif"):
-    x, y = pos
-    # img = PhotoImage(file=file)
-    return _canvas.create_image(x, y, image = tkinter.PhotoImage(file=file), anchor = tkinter.NW)
-
-
 def refresh():
     _canvas.update_idletasks()
-
-def moveCircle(id, pos, r, endpoints=None):
-    global _canvas_x, _canvas_y
-
-    x, y = pos
-#    x0, x1 = x - r, x + r + 1
-#    y0, y1 = y - r, y + r + 1
-    x0, x1 = x - r - 1, x + r
-    y0, y1 = y - r - 1, y + r
-    if endpoints == None:
-        e = [0, 359]
-    else:
-        e = list(endpoints)
-    while e[0] > e[1]: e[1] = e[1] + 360
-
-    if os.path.isfile('flag'):
-        edit(id, ('extent', e[1] - e[0]))
-    else:
-        edit(id, ('start', e[0]), ('extent', e[1] - e[0]))
-    move_to(id, x0, y0)
-
-def edit(id, *args):
-    _canvas.itemconfigure(id, **dict(args))
 
 def text(pos, color, contents, font='Helvetica', size=12, style='normal', anchor="nw"):
     global _canvas_x, _canvas_y
     x, y = pos
     font = (font, str(size), style)
     return _canvas.create_text(x, y, fill=color, text=contents, font=font, anchor=anchor)
-
-def changeText(id, newText, font=None, size=12, style='normal'):
-    _canvas.itemconfigure(id, text=newText)
-    if font != None:
-        _canvas.itemconfigure(id, font=(font, '-%d' % size, style))
-
-def changeColor(id, newColor):
-    _canvas.itemconfigure(id, fill=newColor)
-
-def line(here, there, color=formatColor(0, 0, 0), width=2):
-    x0, y0 = here[0], here[1]
-    x1, y1 = there[0], there[1]
-    return _canvas.create_line(x0, y0, x1, y1, fill=color, width=width)
 
 ##############################################################################
 ### Keypress handling ########################################################
@@ -267,111 +170,11 @@ def _keyrelease(event):
         pass
     _got_release = 1
 
-def remap_arrows(event):
-    # TURN ARROW PRESSES INTO LETTERS (SHOULD BE IN KEYBOARD AGENT)
-    if event.char in ['a', 's', 'd', 'w']:
-        return
-    if event.keycode in [37, 101]: # LEFT ARROW (win / x)
-        event.char = 'a'
-    if event.keycode in [38, 99]: # UP ARROW
-        event.char = 'w'
-    if event.keycode in [39, 102]: # RIGHT ARROW
-        event.char = 'd'
-    if event.keycode in [40, 104]: # DOWN ARROW
-        event.char = 's'
-
 def _clear_keys(event=None):
     global _keysdown, _got_release, _keyswaiting
     _keysdown = {}
     _keyswaiting = {}
     _got_release = None
-
-def keys_pressed(d_o_e=lambda arg: _root_window.dooneevent(arg),
-                 d_w=tkinter._tkinter.DONT_WAIT):
-    d_o_e(d_w)
-    if _got_release:
-        d_o_e(d_w)
-    return _keysdown.keys()
-
-def keys_waiting():
-    global _keyswaiting
-    keys = _keyswaiting.keys()
-    _keyswaiting = {}
-    return keys
-
-# Block for a list of keys...
-
-def wait_for_keys():
-    keys = []
-    while keys == []:
-        keys = keys_pressed()
-        sleep(0.05)
-    return keys
-
-def remove_from_screen(x,
-                       d_o_e=lambda arg: _root_window.dooneevent(arg),
-                       d_w=tkinter._tkinter.DONT_WAIT):
-    _canvas.delete(x)
-    d_o_e(d_w)
-
-def _adjust_coords(coord_list, x, y):
-    for i in range(0, len(coord_list), 2):
-        coord_list[i] = coord_list[i] + x
-        coord_list[i + 1] = coord_list[i + 1] + y
-    return coord_list
-
-def move_to(object, x, y=None,
-            d_o_e=lambda arg: _root_window.dooneevent(arg),
-            d_w=tkinter._tkinter.DONT_WAIT):
-    if y is None:
-        try: x, y = x
-        except: raise  'incomprehensible coordinates'
-
-    horiz = True
-    newCoords = []
-    current_x, current_y = _canvas.coords(object)[0:2] # first point
-    for coord in  _canvas.coords(object):
-        if horiz:
-            inc = x - current_x
-        else:
-            inc = y - current_y
-        horiz = not horiz
-
-        newCoords.append(coord + inc)
-
-    _canvas.coords(object, *newCoords)
-    d_o_e(d_w)
-
-def move_by(object, x, y=None,
-            d_o_e=lambda arg: _root_window.dooneevent(arg),
-            d_w=tkinter._tkinter.DONT_WAIT, lift=False):
-    if y is None:
-        try: x, y = x
-        except: raise Exception('incomprehensible coordinates')
-
-    horiz = True
-    newCoords = []
-    for coord in  _canvas.coords(object):
-        if horiz:
-            inc = x
-        else:
-            inc = y
-        horiz = not horiz
-
-        newCoords.append(coord + inc)
-
-    _canvas.coords(object, *newCoords)
-    d_o_e(d_w)
-    if lift:
-        _canvas.tag_raise(object)
-"""
-def writePostscript(filename):
-    "Writes the current canvas to a postscript file."
-    psfile = open(filename, 'w')
-    psfile.write(_canvas.postscript(pageanchor='sw',
-                     y='0.c',
-                     x='0.c'))
-    psfile.close()"""
 
 def createLetterDict():
     global letterDict
